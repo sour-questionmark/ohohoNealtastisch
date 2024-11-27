d3d033db9fa2b4e6bdd5ccad85872fedf55c4e18
sour-questionmark <soraya.ahmadi@student.tersterre.be>
2024-11-27 09:32:39 +0100

    added all the stupid noises i guess
    sgssfgfsgrgerqyqteytehetghfsdgfhtgh

--- jabanasxhrokn.js
+++ jabanasxhrokn.js
@@ -1,10 +1,94 @@
 // JavaScript Document
 console.log("T WERKT HOOR")
-var isSomethingPlaying=false;
 
-function onClick() {
-	if (isSomethingPlaying==false) {
-		
-	}
+function onClickA() {
 	
+	var audio = new Audio('noise/markPingA.wav');
+	audio.play();
 }
+
+function onClickB() {
+	
+	var audio = new Audio('noise/LizardVoicePinkA.wav');
+	audio.play();
+}
+
+function onClickC() {
+	
+	var audio = new Audio('noise/TH_SU - LEAD.wav');
+	audio.play();
+}
+
+function onClickD() {
+	
+	var audio = new Audio('noise/batChirp1H.wav');
+	audio.play();
+}
+
+function onClickE() {
+	
+	var audio = new Audio('noise/MetalPingA_7.wav');
+	audio.play();
+}
+
+function onClickF() {
+	
+	var audio = new Audio('noise/DigestionInItA_2.wav');
+	audio.play();
+}
+
+function onClickG() {
+	
+	var audio = new Audio('noise/honk3.wav');
+	audio.play();
+}
+
+function onClickH() {
+	
+	var audio = new Audio('noise/puffA_1.wav');
+	audio.play();
+}
+
+function onClickI() {
+	
+	var audio = new Audio('noise/PebblesChat3.wav');
+	audio.play();
+}
+
+function onClickJ() {
+	
+	var audio = new Audio('noise/MoonChat2.wav');
+	audio.play();
+}
+
+function onClickK() {
+	
+	var audio = new Audio('noise/UIImpact1.wav');
+	audio.play();
+}
+
+function onClickL() {
+	
+	var audio = new Audio('noise/machineDOORS.wav');
+	audio.play();
+}
+
+function onClickM() {
+	
+	var audio = new Audio('noise/hydraulicsA_2.wav');
+	audio.play();
+}
+
+function onClickN() {
+	
+	var audio = new Audio('noise/Clang1A.wav');
+	audio.play();
+}
+
+function onClickO() {
+	
+	var audio = new Audio('noise/sof_go2.wav');
+	audio.play();
+}
+
+
